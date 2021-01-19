79fdcf1c20036dd1cbb54cf24373b149274d1112
Oleg Smyrnov <smirnovoleg95@gmail.com>
2021-01-19 19:31:32 +0100

    Зад. 1.1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,28 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+    let algebra = a;
+    let geography = g;
+    let physics = p;// реализуйте здесь задание №1
+    let result = (algebra + geography + physics) / 3;
+
+	return result;// ваш ответ;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+    let myName = userName;// реализуйте здесь задание №2
+    let message = `Привет, мир! Меня зовут ${myName}`;
+
+	return message;// ваш ответ;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2;// реализуйте здесь задание №3
+    let y = 22;
+    let z = 0;
+    let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;// ваш ответ;
 }
