9f695cbacdc9251415d3bc574d49d1b34df977b5
Oleg Smyrnov <smirnovoleg95@gmail.com>
2021-01-20 12:51:44 +0100

    Зад. 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,27 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
     // return totalAmount;
+    "use strict";
+    let perc = percent;
+    let cont = contribution;
+    let price = amount;
+    let bodyCredit = price - cont;
+    let P = perc / 1200;
+    let now = new Date();
+    let endDate = date;
+    let term = (((endDate.getFullYear() - now.getFullYear()) * 12 ) + (endDate.getMonth() - now.getMonth()));
+    let monthAmount = bodyCredit * (P + P / (Math.pow((1+P),term) - 1));
+    let totalAmount = Number((monthAmount * term).toFixed(2));
+    return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     // return greeting;
+    userName = name;
+    if ((userName === undefined) || (userName === ''))
+        userName = 'Аноним';
+    let greeting = `Привет, мир! Меня зовут ${userName}.`
+    console.log(greeting);
+    return greeting;
 }
