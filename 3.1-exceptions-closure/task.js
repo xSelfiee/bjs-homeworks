3920ec79dfaaa7cbf6afd14f41daf75efe47e479
Oleg Smyrnov <smirnovoleg95@gmail.com>
2021-01-24 00:33:40 +0100

    Зад. 3.1

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,40 @@
+function parseCount(num) {
+    const newNum = Number.parseInt(num);
+    if (isNaN(newNum)) {
+        throw new Error("Невалидное значение");;
+    }
+    return newNum;
+}
+function validateCount(num) {
+    try {
+        return parseCount(num);
+    }
+    catch(err) {
+        return err;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if ((this.a + this.b) <= this.c || (this.a + this.c) <= this.b || (this.b + this.c) <= this.a)
+        throw new Error("Треугольник с такими сторонами не существует")
+    }
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+    getArea() {
+        const p = this.getPerimeter() / 2;
+        return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+    }
+}
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } 
+    catch {
+        return {getPerimeter() {return "Ошибка! Треугольник не существует"}, getArea() {return "Ошибка! Треугольник не существует"}};
+    }            
+}
