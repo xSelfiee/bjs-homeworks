9e5754f56b2148e6dd2644f1dbda4ef1a60ace1e
Oleg Smyrnov <smirnovoleg95@gmail.com>
2021-02-16 10:46:38 +0100

    Зад. 3.2

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -1,37 +1,17 @@
 console.clear();
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 function getNames() {
-    let weaponsName = [];
-    for (weapon in weapons)
-    weaponsName.push(weapons[weapon].name);
-    return weaponsName;
+    return weapons.map(weapons => weapons.name);
 }
 function getCountReliableWeapons(durability) {
-    let count = 0;
-    for (weapon in weapons)
-    if (weapons[weapon].durability > durability)
-    count++;
-    return count;
+    return weapons.filter(weapons => weapons.durability > durability).length;
 }
 function hasReliableWeapons(durability) {
-    for (weapon in weapons) {
-        if (weapons[weapon].durability > durability)
-        return true;
-    }
-    return false;
+    return weapons.some(weapons => weapons.durability > durability);    
 }
 function getReliableWeaponsNames(durability) {
-    let weaponsName = [];
-    for (weapon in weapons) {
-        if (weapons[weapon].durability > durability)
-        weaponsName.push(weapons[weapon].name);
-    }
-    return weaponsName;
+    return weapons.filter(weapons => weapons.durability > durability).map(weapons => weapons.name);
 }
 function getTotalDamage() {
-    let sumWeaponsDmg = 0;
-    for (weapon in weapons)
-    sumWeaponsDmg += weapons[weapon].attack;
-    return sumWeaponsDmg;
+    return weapons.reduce((sum, weapons) => sum + weapons.attack, 0);
 }
-//console.log(weapons);
